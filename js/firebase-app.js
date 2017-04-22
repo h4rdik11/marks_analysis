0bb0c0afa67005a0a67cb3dc4dd29e08036e7d32
h4rdik11 <hardik11.chauhan@gmail.com>
2017-04-23 01:00:32 +0530

    Add files via upload

---
+++ js/firebase-app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCMHsx2154JV4AfaKEGDbuidY5XURleFwA",
+    authDomain: "marks-analysis.firebaseapp.com",
+    databaseURL: "https://marks-analysis.firebaseio.com",
+    projectId: "marks-analysis",
+    storageBucket: "marks-analysis.appspot.com",
+    messagingSenderId: "160636005586"
+  };
+  firebase.initializeApp(config);
